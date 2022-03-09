538a4990d1e21150c7a4444edd29cc75ffe1f3c3
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2022-03-09 18:35:26 -0300

    Atualização emergencial.

--- BancoDeDados/hamburgueria.sql
+++ BancoDeDados/hamburgueria.sql
@@ -5,6 +5,7 @@ USE hamburgueria;
 CREATE TABLE pedidos(
 	id INT NOT NULL AUTO_INCREMENT,
     total_price DECIMAL(5,2) NOT NULL,
+    mesa INT NOT NULL,
     id_funcionario INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id)
@@ -13,6 +14,13 @@ CREATE TABLE pedidos(
 CREATE TABLE funcionarios(
 	id INT NOT NULL AUTO_INCREMENT,
     login VARCHAR(20) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    logradouro VARCHAR(60) NOT NULL,
+    logradouro_numero INT NOT NULL,
+    complemento VARCHAR(40) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    celular VARCHAR(19) NOT NULL,
     password VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );
