a9b4999620c0874e3934540e0fcfbdb4c49015c8
Carlos Henrique <ccarloshos2019@gmail.com>
2022-05-10 01:49:01 -0300

    Atualização das telas e otimização de desing

--- react-projeto-Hamburgueria/src/Pages/Cardapio/Cardapio.js
+++ react-projeto-Hamburgueria/src/Pages/Cardapio/Cardapio.js
@@ -6,14 +6,16 @@ const Cardapio = () => {
     return(
         <div>
             <Navbar />
-                <mainCardapio>    
-                    <Card title="meu bug1" img="./banner_background.jpg" descricao="sla testando" />
-                    <Card title="meu bug12" img="./hamBacon.jpg" descricao="sla testando 2" />
-                    <Card title="meu bug12" img="./hamCostela.jpg" descricao="sla testando 2" />
-                    <Card title="meu bug12" img="./hamCheddar.png" descricao="sla testando 2" />
-                    <Card title="meu bug12" img="./hamColosso.jpg" descricao="sla testando 2" />
-                    <Card title="meu bug12" img="./hamPeru.jpg" descricao="sla testando 2" />
-                    <Card title="meu bug12" img="./hamFrango.jpg" descricao="sla testando 2" />
+                <mainCardapio>
+                    <div className="container">
+                        <Card title="meu bug1" img="./banner_background.jpg" descricao="sla testando" />
+                        <Card title="meu bug12" img="./hamBacon.jpg" descricao="sla testando 2" />
+                        <Card title="meu bug12" img="./hamCostela.jpg" descricao="sla testando 2" />
+                        <Card title="meu bug12" img="./hamCheddar.png" descricao="sla testando 2" />
+                        <Card title="meu bug12" img="./hamColosso.jpg" descricao="sla testando 2" />
+                        <Card title="meu bug12" img="./hamPeru.jpg" descricao="sla testando 2" />
+                        <Card title="meu bug12" img="./hamFrango.jpg" descricao="sla testando 2" />  
+                    </div> 
                 </mainCardapio>
         </div>
     );
